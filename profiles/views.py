be74f346de0376f8a3f78939fd5735fe3b9baa8a
okurut <henrykedi.okurut@netcompany-intrasoft.com>
2023-09-24 06:02:02 +0200

    Create views and ursl patterns for the profiles app

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Profiles
 
 # Create your views here.
+class ProfileList(APIView):
+    def get(self, request):
+        profiles = Profiles.objects.all()
+        return Response(profiles)

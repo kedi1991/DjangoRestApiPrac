cf917083a2aa8cd48d6f34ddbf028a4ec4ce1e39
okurut <henrykedi.okurut@netcompany-intrasoft.com>
2023-09-25 02:41:03 +0200

    Add post view API

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Post
+from rest_framework import status
+from django.http import Http404
+from .serializers import PostSerializers
+from drf_api.permissions import IsOwnerOrReadOnly
 
 # Create your views here.
+class PostList(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializers(posts, many=True, context={'request': request})
+        return Response(serializer.data)
+    
